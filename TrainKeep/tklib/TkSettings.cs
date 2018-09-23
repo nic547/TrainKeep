a66edd997be31d0c427a0ae18b7f2b68dc9bd4c9
nic547 <nic547@users.noreply.github.com>
2018-09-23 20:24:00 +0200

    Add the Xamarin.Forms project and the Settings page

---
+++ TrainKeep/tklib/TkSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace tklib
+{
+    public class TkSettings
+    {
+        public static string ConnectionString { get; set; }
+
+        public static void SetConnectionString(string ip, string db, string user, string pw)
+        {
+            ConnectionString = BuildConnectionString(ip,db,user,pw);
+        }
+        public static string BuildConnectionString(string ip, string db, string user, string pw)
+        {
+            return $"Server={ip};User Id={user};Password={pw};Database={db};SSL Mode=Prefer;Trust Server Certificate=true;Application Name=TrainKeep;";
+        }
+    }
+}
