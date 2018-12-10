d08a8a3fe5032468de86ac1188b2d54bf865e7d0
nic547 <nic547@users.noreply.github.com>
2018-12-10 17:16:19 +0100

    update connection string
    -Use Password
    -Prefer SSL
    -Trust Server Certificate
    -Use Pooling
    -Use Autoprepare for 20 queries
    -Use the Application name

--- TkLib.Dal/TrainKeepContext.cs
+++ TkLib.Dal/TrainKeepContext.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 
 namespace TkLib.Dal
 {
-    public class TrainKeepContext: DbContext
+    public class TrainKeepContext : DbContext
     {
         static public string Hostname { get; set; }
         static public string DatabaseName { get; set; }
@@ -15,13 +15,14 @@ namespace TkLib.Dal
 
 
         override protected void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-            => optionsBuilder.UseNpgsql($"Host={Hostname};Database={DatabaseName};Username={Username};");
+            => optionsBuilder.UseNpgsql($"Host={Hostname};Database={DatabaseName};Username={Username};Password={Password};SSL Mode=Prefer;Trust Server Certificate=true;Pooling=true;Max Auto Prepare=20;Application Name=TrainKeep;");
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Item>().ToTable("items");
             modelBuilder.Entity<ItemModel>().ToTable("item_models");
             modelBuilder.Entity<Manufacturer>().ToTable("manufacturer");
+            modelBuilder.Entity<Prototype>().ToTable("prototype");
         }
 
         public DbSet<Item> Items { get; set; }
