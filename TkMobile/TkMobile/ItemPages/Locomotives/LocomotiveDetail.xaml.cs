73699bf97eab65a5a41a40b277561435e4c3c8a9
nic547 <7825979+nic547@users.noreply.github.com>
2020-02-11 22:51:36 +0100

    Further work on the loco detail screen

--- TkMobile/TkMobile/ItemPages/Locomotives/LocomotiveDetail.xaml.cs
+++ TkMobile/TkMobile/ItemPages/Locomotives/LocomotiveDetail.xaml.cs
@@ -20,21 +20,30 @@ namespace TkMobile.ItemPages
 #pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 #pragma warning restore SA1601 // Partial elements should be documented
     {
+        private Item item;
+
+        private Model model;
+
+        private Prototype prototype;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="LocomotiveDetail"/> class.
         /// </summary>
         /// <param name="item">The <see cref="Item"/> for which the detail page should be shown.</param>
         public LocomotiveDetail(Item item)
         {
-            Item = item;
-            BindingContext = Item;
+            this.item = item;
+            BindingContext = this.item;
+            model = item.Model;
+            prototype = item?.Model?.Prototype;
+
             InitializeComponent();
+
+            ModelSection.BindingContext = model;
+            ProtoSection.BindingContext = prototype;
+
             ImageThing.Source = ImageSource.FromResource("TkMobile.DefaultImage.jpg");
         }
-
-        private Item Item { get; }
-        private Model model { get; set; }
-        private Prototype prototype { get; set; }
 
         private Database Database { get; } = DatabaseManager.GetDatabase();
 
@@ -42,30 +51,29 @@ namespace TkMobile.ItemPages
         protected async override void OnAppearing()
         {
             ProtoPicker.ItemsSource = Database.Locomotives.Prototypes.Values.ToList();
-            ProtoPicker.SelectedItem = Item?.Model?.Prototype;
+            ProtoPicker.SelectedItem = item?.Model?.Prototype;
 
             // Not done via bindings because I'm not smart enough to stop it from displaying zero that way.
-            DCCCell.Text = Item.Dcc != 0 ? Item.Dcc.ToString() : string.Empty;
+            ItemDccCell.Text = item.Dcc != 0 ? item.Dcc.ToString() : string.Empty;
 
             UpdateModelSelector();
+            ModelPicker.SelectedItem = item?.Model;
 
-            await Database.Locomotives.LoadImage(Item);
+            await Database.Locomotives.LoadImage(item);
 
-            if (Item.Image != null)
+            if (item.Image != null)
             {
-                ImageThing.Source = ImageSource.FromStream(() => new MemoryStream(Item.Image.ToArray()));
+                ImageThing.Source = ImageSource.FromStream(() => new MemoryStream(item.Image.ToArray()));
             }
         }
 
         private void UpdateModelSelector()
         {
             ModelPicker.ItemsSource = Database.Locomotives.Models.Values.Where(x => x.Prototype == ProtoPicker.SelectedItem).ToList();
-            ModelPicker.SelectedItem = Item?.Model;
         }
 
         private void ProtoPicker_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
             UpdateModelSelector();
         }
 
@@ -87,6 +95,36 @@ namespace TkMobile.ItemPages
             }
 
             await Navigation.PushModalAsync(new PromptPage("Enter the name of the prototype", Action));
+        }
+
+        private void SaveButton_Clicked(object sender, EventArgs e)
+        {
+            item.Name = ItemNameCell.Text;
+            try
+            {
+                item.Dcc = short.Parse(ItemDccCell.Text);
+            }
+            catch
+            {
+            }
+
+            item.Notes = ItemNotesEditor.Text;
+            item.Model = model;
+
+            if (item.Id == 0)
+            {
+                Database.Locomotives.Insert(item);
+            }
+        }
+
+        private void CloseButton_Clicked(object sender, EventArgs e)
+        {
+        }
+
+        private void ModelPicker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            model = (Model)ModelPicker.SelectedItem;
+            ModelSection.BindingContext = model;
         }
     }
 }
