f029fae5e675f3a55069ab9905a0e39a0faece19
nic547 <7825979+nic547@users.noreply.github.com>
2020-03-10 12:37:01 +0100

    DefaultImage is now 16:9, linked not duplicated and referenced in xaml
    16:9 is a better fit for almost all engines and carriages.
    
    The Image used to be both in the resource folder and in the TkMobile project, now it's a link from the the project to the resource folder.
    
    The ImageResourceExtension is as described in the offical Xamarin Forms documentation in order to load an image in XAML from resources. The Purpose of this is to avoid setting the default Image after the Page is already open, not changing the layout.

---
+++ TkMobile/TkMobile/ImageResourceExtension.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Dominic Ritz. All Rights Reserved.
+// Licensed under the GNU GPL, Version 3.0 or any later version. See LICENSE in the project root for license information.
+
+namespace TkMobile
+{
+    using System;
+    using System.Reflection;
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    /// <summary>
+    /// Provides an Image from a resource (for XAML).
+    /// </summary>
+    [ContentProperty(nameof(Source))]
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        /// <summary>
+        /// Gets or sets the resource identifier.
+        /// </summary>
+        public string Source { get; set; }
+
+        /// <inheritdoc/>
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+            {
+                return null;
+            }
+
+            // Do your translation lookup here, using whatever method you require
+            var imageSource = ImageSource.FromResource(Source, typeof(ImageResourceExtension).GetTypeInfo().Assembly);
+
+            return imageSource;
+        }
+    }
+}
